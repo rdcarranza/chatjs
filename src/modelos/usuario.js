00ff67ce3facf99b78ab851e69970581adf96f60
Dario Carranza <dario_zmd@yahoo.com.ar>
2021-01-22 12:04:20 +0000

    Generando registro de usuario.

--- src/modelos/usuario.js
+++ src/modelos/usuario.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const usuarioEsquema = new Schema({
-    usuario_id: {type: mongoose.ObjectId, default: new mongoose.Types.ObjectId(), unique: true},
+    //usuario_id: {type: mongoose.ObjectId, default: new mongoose.Types.ObjectId(), unique: true},
+    //usuario_id:{type: Number},
     nombre: String,
     conexion_id: Number,
     fecha_creado:{type: Date, default: Date.now}
