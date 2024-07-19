d227e0655ebb25ef315fd609f38e5a529b7cddfa
Dario Carranza <dario_zmd@yahoo.com.ar>
2024-07-19 01:17:09 -0300

    Se agrega documentación.

---
+++ src/modelos/conexion.js
@@ -0,0 +1,4 @@
+//La conexión es la clase que agrupa (agregación) dos objetos de las clases Usuario y Token.
+// Una conexión se genera cuando el usuario registra un nombre valido.
+
+//Revisar la logica, si es posible reemplazar el modelo usuario por esta clase o dividirla.
